d83763f2c51299c743c1fae1db11f06cacfc3ea5
Jess <146346577+JessFe@users.noreply.github.com>
2023-12-22 17:50:58 +0100

    carousel autoplay

--- src/components/MovieGallery.jsx
+++ src/components/MovieGallery.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { Navigation, Pagination, Scrollbar, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 // import "swiper/css/pagination";
 // import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 
 const MovieGallery = ({ title, searchQuery }) => {
   const [movies, setMovies] = useState([]);
@@ -44,10 +45,16 @@ const MovieGallery = ({ title, searchQuery }) => {
     <>
       <h4 className="text-light mb-2 mt-4">{title}</h4>
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, Autoplay]}
         spaceBetween={10}
         slidesPerView={6}
         navigation
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        speed={1500}
         breakpoints={{
           640: {
             slidesPerView: 2,
